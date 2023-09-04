eaf58de3e8c67beff836090f7afb8dd74ee25e08
CodeSandbox <support@codesandbox.io>
2023-09-04 15:04:59 +0000

    Initial commit

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
+    res.write("See server console!"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
+
+
+
+// Code start from here - 
+
+
+// Js Memorization 
+
+
+const cache = new Map()
+const heavyTask = (n) => {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += i + 1
+  }
+  return sum
+
+}
+const merorizeFn = (fn) => {
+
+  return (size) => {
+
+    if (cache.get(size)) {
+      console.log(`${size} from cache`)
+      return fn(size)
+    }
+
+    const res = heavyTask(size)
+    cache.set(size, res)
+    return fn(size)
+  }
+}
+
+
+//Code 
+const memo = merorizeFn((size) => {
+  console.log("Memorization Run for size ", size)
+})
+
+console.time("Time 1")
+memo(200000)
+console.timeEnd("Time 1")
+
+console.log("\n")
+
+console.time("Time 2")
+memo(200000)
+console.timeEnd("Time 2")
